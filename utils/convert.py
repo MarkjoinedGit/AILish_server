6f1855fb9d07a1c2a83810b7e89be5c11b61f174
Anh Khoa Mark <qazblp2773652@gmail.com>
2024-04-26 06:13:04 +0700

    fix convert

--- utils/convert.py
+++ utils/convert.py
@@ -1,16 +1,21 @@
-
 def convert_result_api_aictionary_to_vocabylary(data):
     word = data["word"]
-    phonetics = data["phonetics"]
-    audio_url = [phonetic["audio"] for phonetic in data["phonetics"] if phonetic["audio"]]
+    phonetics = data.get("phonetics", [])
+    pronunciation_audio = [pronunciation.get("audio", "") for pronunciation in phonetics]
+    id_to_get_audio = 0
+
+
+    audioSrc = pronunciation_audio[id_to_get_audio] if phonetics else ""
+    pronunciation_text = phonetics[id_to_get_audio].get("text") if phonetics else ""
+    
     definitions = [meaning["definition"] for meaning in data["meanings"][0]["definitions"] if "definition" in meaning and meaning["definition"]]
     examples = [meaning["example"] for meaning in data["meanings"][0]["definitions"] if "example" in meaning and meaning["example"]]
     part_of_speech = data["meanings"][0]["partOfSpeech"]
     return {
-            'word': word,
-            'definitions':definitions,
-            'pronunciation': phonetics,
             'category': part_of_speech,
-            'audioSrc': audio_url,
-            'example':examples
+            'word': word,
+            'definitions': definitions,
+            'pronunciation':  pronunciation_text,  # Lấy giá trị text đầu tiên trong chuỗi pronunciations
+            'audioSrc': audioSrc,  # Sử dụng danh sách audio từ chuỗi pronunciations
+            'example': examples
         }
